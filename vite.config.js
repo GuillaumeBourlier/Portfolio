bd3aa9b53a92488ace7bc350bb8989ba0d243034
VotreNom <VotreEmail@example.com>
2025-07-29 14:41:29 +0200

    modif

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Portfolio/",
   plugins: [react()],
-  base: '/Portfolio/',
   css: {
     preprocessorOptions: {
       scss: {
@@ -17,7 +17,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
-})
+});
