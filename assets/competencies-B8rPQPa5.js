5645f7fe0f94906d3750e6595deb74749fd14012
VotreNom <VotreEmail@example.com>
2025-08-14 16:52:19 +0200

    Updates

--- assets/competencies-B8rPQPa5.js
+++ assets/competencies-B8rPQPa5.js
@@ -1,4 +1,4 @@
-import{f as h,N as j,L as O,j as e,F as f,a as C,b as E}from"./main-C8FYVmrt.js";/*! *****************************************************************************
+import{f as h,N as j,L as O,j as e,F as f,a as C,b as E}from"./main-C8BZHFSC.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
