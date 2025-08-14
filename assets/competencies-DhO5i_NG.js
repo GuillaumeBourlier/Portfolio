766940747f451d7d793cc94030f5180b689d7f13
VotreNom <VotreEmail@example.com>
2025-08-14 17:12:19 +0200

    Updates

--- assets/competencies-DhO5i_NG.js
+++ assets/competencies-DhO5i_NG.js
@@ -1,4 +1,4 @@
-import{f as h,N as j,L as O,j as e,F as f,a as C,b as E}from"./main--POfiN8l.js";/*! *****************************************************************************
+import{f as h,N as j,L as O,j as e,F as f,a as C,b as E}from"./main-OMb9XsnQ.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
